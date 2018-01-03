266e8834f34575f665b97e34bb80aebfdc6d39c8
antonio <antoniobelmardacosta@gmail.com>
2018-01-03 13:41:53 +0000

    Add tests for decrease in quality of a general item before and after sell-in date

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -6,7 +6,17 @@ describe GildedRose do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      expect(items[0].name).to eq "foo"
+    end
+    it "reduces quality of a non-edge case item by 1" do
+      items = [Item.new("foo", 10, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(9)
+    end
+    it "reduces quality of a non-edge case item by 2 once sell by date has passed" do
+      items = [Item.new("foo", -1, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(8)
     end
   end
 
