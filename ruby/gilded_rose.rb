8ee8b8737d20e2c70db76c085f97d745e5a4c623
antonio <antoniobelmardacosta@gmail.com>
2018-01-03 18:09:44 +0000

    Replace magic numbers for quality by constants in update quality method

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,4 +1,6 @@
 class GildedRose
+  MIN_QUALITY = 0
+  MAX_QUALITY = 50
 
   def initialize(items)
     @items = items
@@ -7,17 +9,17 @@ class GildedRose
   def update_quality()
     @items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
+        if item.quality > MIN_QUALITY
           if item.name != "Sulfuras, Hand of Ragnaros"
             item.quality = item.quality - 1
           end
         end
       else
-        if item.quality < 50
+        if item.quality < MAX_QUALITY
           item.quality = item.quality + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
-              if item.quality < 50
+              if item.quality < MAX_QUALITY
                 item.quality = item.quality + 1
               end
             end
@@ -35,7 +37,7 @@ class GildedRose
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
+            if item.quality > MIN_QUALITY
               if item.name != "Sulfuras, Hand of Ragnaros"
                 item.quality = item.quality - 1
               end
@@ -44,7 +46,7 @@ class GildedRose
             item.quality = item.quality - item.quality
           end
         else
-          if item.quality < 50
+          if item.quality < MAX_QUALITY
             item.quality = item.quality + 1
           end
         end
