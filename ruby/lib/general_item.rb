970e4ee4fcc7470bcbec3f4df28e290da84a36a1
antonio <antoniobelmardacosta@gmail.com>
2018-01-04 16:37:41 +0000

    Complete method for update quality and sell-in for general item class

--- ruby/lib/general_item.rb
+++ ruby/lib/general_item.rb
@@ -7,13 +7,13 @@ class General_Item
     if @item.quality > 0
       @item.quality -=1
     end
-    if @item.sell_in < 0 && item.quality > 0
+    if @item.sell_in < 0 && @item.quality > 0
       @item.quality -=1
     end
   end
 
   def update_sell_in
-    item.sell_in -= 1
+    @item.sell_in -= 1
   end
 
 end
--- ruby/spec/general_item_spec.rb
+++ ruby/spec/general_item_spec.rb
@@ -9,5 +9,25 @@ describe 'General Item' do
       item_type.update_quality
       expect(item.quality).to eq(9)
     end
+    it "reduces quality of a non-edge case item by 2 once sell by date has passed" do
+      item = Item.new("foo", -1, 10)
+      item_type = General_Item.new(item)
+      item_type.update_quality
+      expect(item.quality).to eq(8)
+    end
+    it "ensures quality of an item is never negative" do
+      item = Item.new("foo", -1, 0)
+      item_type = General_Item.new(item)
+      item_type.update_quality
+      expect(item.quality).to eq(0)
+    end
+  end
+  describe 'update_sell_in' do
+    it 'reduces sell-in date by 1' do
+      item = Item.new("foo", 8, 10)
+      item_type = General_Item.new(item)
+      item_type.update_sell_in
+      expect(item.sell_in).to eq(7)
+    end
   end
 end
