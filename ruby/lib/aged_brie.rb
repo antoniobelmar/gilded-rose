643d004669628b477034a8a4018c84e2ec820940
antonio <antoniobelmardacosta@gmail.com>
2018-01-04 18:40:43 +0000

    Remove magic numbers from aged brie class

--- ruby/lib/aged_brie.rb
+++ ruby/lib/aged_brie.rb
@@ -1,16 +1,20 @@
 class Aged_Brie
+  MAX_QUALITY = 50
+  QUALITY_INCREMENT = 1
+  SELL_IN_INCREMENT = 1
+
   def initialize(item)
     @item = item
   end
 
   def update_quality
-    if @item.quality < 50
-      @item.quality+=1
+    if @item.quality < MAX_QUALITY
+      @item.quality +=QUALITY_INCREMENT
     end
   end
 
   def update_sell_in
-    @item.sell_in -= 1
+    @item.sell_in -= SELL_IN_INCREMENT
   end
 
 end
